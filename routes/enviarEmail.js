e6616e550beb863065cae106b125b1ad1d24e7b4
facundolizardo75@gmail.com <facundolizardo75@gmail.com>
2024-12-06 11:17:39 -0300

    logs

--- routes/enviarEmail.js
+++ routes/enviarEmail.js
@@ -83,6 +83,7 @@ router.post("/", async (req, res) => {
     const info = await transporter.sendMail(mailOptions);
     res.status(200).json({ message: "Correo enviado con éxito", info });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Error al enviar el correo", error });
   }
 });
